64bb4c14694960684fce64a51adf408631b57865
TheFirstGomes <lggomesconsul@gmail.com>
2025-10-10 22:31:11 -0300

    "Atualização do projeto"

--- ALURA/01_Curso_PrimeiroProjeto/app.py
+++ ALURA/01_Curso_PrimeiroProjeto/app.py
@@ -1,1 +1,27 @@
-print("Hello world!")
+print('''
+
+██████████████████████████████████████████████████████████████████████████
+█─▄▄▄▄██▀▄─██▄─▄─▀█─▄▄─█▄─▄▄▀███▄─▄▄─█▄─▀─▄█▄─▄▄─█▄─▄▄▀█▄─▄▄─█─▄▄▄▄█─▄▄▄▄█
+█▄▄▄▄─██─▀─███─▄─▀█─██─██─▄─▄████─▄█▀██▀─▀███─▄▄▄██─▄─▄██─▄█▀█▄▄▄▄─█▄▄▄▄─█
+▀▄▄▄▄▄▀▄▄▀▄▄▀▄▄▄▄▀▀▄▄▄▄▀▄▄▀▄▄▀▀▀▄▄▄▄▄▀▄▄█▄▄▀▄▄▄▀▀▀▄▄▀▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀
+''')
+
+print('1. Cadastrar restaurante')
+print('2. Listar restaurante')
+print('3. Ativar restaurante')
+print('4. Sair\n')
+
+opcao_escolhida = int(input('Escolha uma opção: '))
+print(f'Você escolheu a opção {opcao_escolhida}\n')
+
+match opcao_escolhida: 
+    case 1: 
+        print('Cadastrar restaurantes\n')
+    case 2: 
+        print('Listar restaurantes\n')
+    case 3: 
+        print('Ativar restaurantes\n')
+    case 4: 
+        print('Encerrando o sistema, obrigado por utilizar nossos sistemas!\n')
+    case _: 
+        print('Opção incorreta. Digite novamente\n')
